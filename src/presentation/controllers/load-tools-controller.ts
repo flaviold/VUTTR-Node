354d2c3d27f322e7b10eec540277cb8814771075
Flavio Duarte <flduarte13@gmail.com>
2020-12-23 13:33:19 -0300

    feat: ensure LoadToolsController returns 200 if LoadTools returns a list of tools

--- src/presentation/controllers/load-tools-controller.ts
+++ src/presentation/controllers/load-tools-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { LoadTools } from '@/domain/usecases/load-tools'
+import { ok } from '@/presentation/helpers'
 
 export class LoadToolsController implements Controller {
   constructor (
@@ -7,10 +8,7 @@ export class LoadToolsController implements Controller {
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    await this.loadTools.load(request.query?.tag)
-    return {
-      statusCode: 200,
-      body: {}
-    }
+    const tools = await this.loadTools.load(request.query?.tag)
+    return ok(tools)
   }
 }
