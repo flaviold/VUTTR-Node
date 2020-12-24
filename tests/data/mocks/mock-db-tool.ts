04201e388a4f81831bdec6d9d770b4dee4b3ea05
Flavio Duarte <flduarte13@gmail.com>
2020-12-23 22:11:52 -0300

    test: ensure DbLoadTools calls LoadToolByTagsRepository when necessary

--- tests/data/mocks/mock-db-tool.ts
+++ tests/data/mocks/mock-db-tool.ts
@@ -1,4 +1,4 @@
-import { LoadToolsRepository } from '@/data/protocols/load-tools-repository'
+import { LoadToolByTagsRepository, LoadToolsRepository } from '@/data/protocols'
 import { ToolModel } from '@/domain/models/tool'
 import { makeTools } from '@/tests/domain/mocks/mock-tool'
 
@@ -6,6 +6,16 @@ export class LoadToolsRepositorySpy implements LoadToolsRepository {
   result = makeTools()
 
   async load (): Promise<ToolModel[]> {
+    return this.result
+  }
+}
+
+export class LoadToolByTagsRepositorySpy implements LoadToolByTagsRepository {
+  tags?: string[]
+  result = makeTools()
+
+  async loadByTags (tags: string[]): Promise<ToolModel[]> {
+    this.tags = tags
     return this.result
   }
 }
--- tests/data/usecases/db-load-tools.spec.ts
+++ tests/data/usecases/db-load-tools.spec.ts
@@ -1,32 +1,49 @@
 import { DbLoadTools } from '@/data/usecases/db-load-tools'
-import { LoadToolsRepositorySpy } from '@/tests/data/mocks/mock-db-tool'
+import { LoadToolByTagsRepositorySpy, LoadToolsRepositorySpy } from '@/tests/data/mocks/mock-db-tool'
 
 interface SutTypes {
   sut: DbLoadTools
-  loadToolsRepository: LoadToolsRepositorySpy
+  loadToolsRepositorySpy: LoadToolsRepositorySpy
+  loadToolByTagsRepositorySpy: LoadToolByTagsRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
-  const loadToolsRepository = new LoadToolsRepositorySpy()
-  const sut = new DbLoadTools(loadToolsRepository)
+  const loadToolsRepositorySpy = new LoadToolsRepositorySpy()
+  const loadToolByTagsRepositorySpy = new LoadToolByTagsRepositorySpy()
+  const sut = new DbLoadTools(loadToolsRepositorySpy, loadToolByTagsRepositorySpy)
   return {
     sut,
-    loadToolsRepository
+    loadToolsRepositorySpy,
+    loadToolByTagsRepositorySpy
   }
 }
 
 describe('DbLoadTools', () => {
   test('Should call LoadToolsRepository if no tag is provided', async () => {
-    const { sut, loadToolsRepository } = makeSut()
-    const loadSpy = jest.spyOn(loadToolsRepository, 'load')
+    const { sut, loadToolsRepositorySpy } = makeSut()
+    const loadSpy = jest.spyOn(loadToolsRepositorySpy, 'load')
     await sut.load()
     expect(loadSpy).toHaveBeenCalledTimes(1)
   })
 
   test('Should not call LoadToolsRepository if tag is provided', async () => {
-    const { sut, loadToolsRepository } = makeSut()
-    const loadSpy = jest.spyOn(loadToolsRepository, 'load')
+    const { sut, loadToolsRepositorySpy } = makeSut()
+    const loadSpy = jest.spyOn(loadToolsRepositorySpy, 'load')
     await sut.load('any_tag')
     expect(loadSpy).toHaveBeenCalledTimes(0)
+  })
+
+  test('Should call LoadToolByTagsRepository if tag is provided', async () => {
+    const { sut, loadToolByTagsRepositorySpy } = makeSut()
+    const loadByTagsSpy = jest.spyOn(loadToolByTagsRepositorySpy, 'loadByTags')
+    await sut.load('any_tag')
+    expect(loadByTagsSpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should not call LoadToolByTagsRepository if no tag is provided', async () => {
+    const { sut, loadToolByTagsRepositorySpy } = makeSut()
+    const loadByTagsSpy = jest.spyOn(loadToolByTagsRepositorySpy, 'loadByTags')
+    await sut.load()
+    expect(loadByTagsSpy).toHaveBeenCalledTimes(0)
   })
 })
