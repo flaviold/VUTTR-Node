ea59fc429faa055cedb85c247fee03c02221f4d3
Flavio Duarte <flduarte13@gmail.com>
2020-12-29 06:31:55 -0300

    feat: ensure ToolMongoRepository.loadByTags returns all tools with the provided tags

--- src/infra/db/mongodb/tool-mongo-repository.ts
+++ src/infra/db/mongodb/tool-mongo-repository.ts
@@ -1,11 +1,17 @@
-import { LoadToolsRepository } from '@/data/protocols'
+import { LoadToolByTagsRepository, LoadToolsRepository } from '@/data/protocols'
 import { ToolModel } from '@/domain/models/tool'
 import { MongoHelper } from './mongo-helper'
 
-export class ToolMongoRepository implements LoadToolsRepository {
+export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsRepository {
   async load (): Promise<ToolModel[]> {
     const toolCollection = await MongoHelper.getCollection('tools')
     const tools = await toolCollection.find().toArray()
+    return MongoHelper.mapList(tools)
+  }
+
+  async loadByTags (tags: string[]): Promise<ToolModel[]> {
+    const toolCollection = await MongoHelper.getCollection('tools')
+    const tools = await toolCollection.find({ tags: { $all: tags } }).toArray()
     return MongoHelper.mapList(tools)
   }
 }
