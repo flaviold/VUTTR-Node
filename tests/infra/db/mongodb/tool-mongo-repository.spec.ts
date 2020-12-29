13241b6b92e77b8a0c7f0b38bb7ebb03107aa7fa
Flavio Duarte <flduarte13@gmail.com>
2020-12-29 06:23:02 -0300

    test: ensure ToolMongoRepository.load returns all tools on success

---
+++ tests/infra/db/mongodb/tool-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { ToolMongoRepository } from '@/infra/db/mongodb/tool-mongo-repository'
+import { addTools } from '@/tests/domain/mocks/mock-tool'
+
+let toolCollection: Collection
+
+const makeSut = (): ToolMongoRepository => {
+  return new ToolMongoRepository()
+}
+
+describe('ToolMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  beforeEach(async () => {
+    toolCollection = await MongoHelper.getCollection('tools')
+    await toolCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('load()', () => {
+    test('Should return all tools on success', async () => {
+      const sut = makeSut()
+      await toolCollection.insertMany(addTools)
+      const tools = await sut.load()
+      expect(tools.length).toBe(3)
+      expect(tools[0].id).toBeTruthy()
+      expect(tools[0].title).toBe(addTools[0].title)
+      expect(tools[0].link).toBe(addTools[0].link)
+      expect(tools[0].description).toBe(addTools[0].description)
+      expect(tools[0].tags).toEqual(addTools[0].tags)
+    })
+  })
+})
