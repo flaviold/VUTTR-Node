bb1e96246bbf67d1d880fee206da4929bbd69235
Flavio Duarte <flduarte13@gmail.com>
2020-12-30 06:00:44 -0300

    feat: ensure CORS is enable

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import setupMiddlewares from './middlewares'
 
 const app = express()
+
+setupMiddlewares(app)
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello!' })
