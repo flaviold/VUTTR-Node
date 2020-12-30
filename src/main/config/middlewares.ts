c10573da44be5e166898025a750528a80206ff9d
Flavio Duarte <flduarte13@gmail.com>
2020-12-30 06:15:34 -0300

    feat: ensure requests parse body as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express'
-import { cors } from '../middlewares'
+import { bodyParser, cors } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(cors)
+  app.use(bodyParser)
 }
