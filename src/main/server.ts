95915db3cf2af9beedb851d5283239cb342a4938
Flavio Duarte <flduarte13@gmail.com>
2020-12-30 05:59:39 -0300

    chore: add express

---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import 'module-alias/register'
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const { default: app } = await import('./config/app')
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
