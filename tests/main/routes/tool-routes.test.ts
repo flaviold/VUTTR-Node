4cc97c959210a1c3616ea83ac95c47d2ce223ad5
Flavio Duarte <flduarte13@gmail.com>
2020-12-30 22:05:18 -0300

    test: ensure tool router returns 200 when there are tools to return

--- tests/main/routes/tool-routes.test.ts
+++ tests/main/routes/tool-routes.test.ts
@@ -1,10 +1,15 @@
 import app from '@/main/config/app'
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { addTools } from '@/tests/domain/mocks/mock-tool'
 
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
 let toolCollection: Collection
+
+const insertTools = async (): Promise<void> => {
+  await toolCollection.insertMany(addTools)
+}
 
 describe('Tool Routes', () => {
   beforeAll(async () => {
@@ -16,11 +21,18 @@ describe('Tool Routes', () => {
   })
 
   beforeEach(async () => {
-    toolCollection = await MongoHelper.getCollection('accounts')
+    toolCollection = await MongoHelper.getCollection('tools')
     await toolCollection.deleteMany({})
   })
 
   describe('GET /tools', () => {
+    test('Should return 200 when there are tools to return', async () => {
+      await insertTools()
+      await request(app)
+        .get('/tools')
+        .expect(200)
+    })
+
     test('Should return 204 when empty', async () => {
       await request(app)
         .get('/tools')
