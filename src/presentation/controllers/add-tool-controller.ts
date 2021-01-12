7ad87e84bdd7dd6ccd7f0805f179e49c6ec205f1
Flavio Duarte <flduarte13@gmail.com>
2021-01-11 22:29:00 -0300

    feat: ensure AddToolController calls Validation with correct values

--- src/presentation/controllers/add-tool-controller.ts
+++ src/presentation/controllers/add-tool-controller.ts
@@ -1,12 +1,14 @@
 import { AddTool } from '@/domain/usecases'
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AddToolController implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly addTool: AddTool
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(request.body)
     await this.addTool.add(request.body)
     return null
   }
