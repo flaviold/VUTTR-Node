65dadcf60dab770959eefb6dec9363c08c67fef2
Flavio Duarte <flduarte13@gmail.com>
2021-01-14 22:06:14 -0300

    feat: ensure ToolMongoRepository.add adds a tool on success

--- src/infra/db/mongodb/tool-mongo-repository.ts
+++ src/infra/db/mongodb/tool-mongo-repository.ts
@@ -1,8 +1,9 @@
-import { LoadToolByTagsRepository, LoadToolsRepository } from '@/data/protocols'
+import { AddToolRepository, LoadToolByTagsRepository, LoadToolsRepository } from '@/data/protocols'
 import { ToolModel } from '@/domain/models/tool'
+import { AddToolModel } from '@/domain/usecases'
 import { MongoHelper } from './mongo-helper'
 
-export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsRepository {
+export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsRepository, AddToolRepository {
   async load (): Promise<ToolModel[]> {
     const toolCollection = await MongoHelper.getCollection('tools')
     const tools = await toolCollection.find().toArray()
@@ -13,5 +14,11 @@ export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsR
     const toolCollection = await MongoHelper.getCollection('tools')
     const tools = await toolCollection.find({ tags: { $all: tags } }).toArray()
     return MongoHelper.mapList(tools)
+  }
+
+  async add (toolData: AddToolModel): Promise<ToolModel> {
+    const toolCollection = await MongoHelper.getCollection('tools')
+    await toolCollection.insertOne(toolData)
+    return null
   }
 }
