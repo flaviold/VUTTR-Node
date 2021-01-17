df4db52292e64be6fb6f2965c6e881c85c463398
Flavio Duarte <flduarte13@gmail.com>
2021-01-17 18:44:58 -0300

    test: ensure RemoveToolController returns 400 if Validation returns an error

--- tests/presentation/controllers/remove-tool-controller.spec.ts
+++ tests/presentation/controllers/remove-tool-controller.spec.ts
@@ -1,7 +1,15 @@
 import { RemoveToolController } from '@/presentation/controllers'
 import { ValidationSpy } from '@/tests/presentation/mocks'
+import { HttpRequest } from '@/presentation/protocols'
+import { badRequest } from '@/presentation/helpers'
 
 import faker from 'faker'
+
+const makeRequest = (): HttpRequest => ({
+  params: {
+    id: faker.random.uuid()
+  }
+})
 
 interface SutTypes {
   sut: RemoveToolController
@@ -20,12 +28,15 @@ const makeSut = (): SutTypes => {
 describe('RemoveToolController', () => {
   test('Should call Validation with correct input', async () => {
     const { sut, validationSpy } = makeSut()
-    const httpRequest = {
-      params: {
-        id: faker.random.uuid()
-      }
-    }
+    const httpRequest = makeRequest()
     await sut.handle(httpRequest)
     expect(validationSpy.input).toEqual(httpRequest.params)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(makeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
