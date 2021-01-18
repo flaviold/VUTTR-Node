87f36b8aafcdfc67240412136fa1853d825180a4
Flavio Duarte <flduarte13@gmail.com>
2021-01-17 21:43:53 -0300

    feat: ensure ToolMongoRepository.remove removes the tool on sucess

--- src/infra/db/mongodb/tool-mongo-repository.ts
+++ src/infra/db/mongodb/tool-mongo-repository.ts
@@ -1,9 +1,9 @@
-import { AddToolRepository, LoadToolByTagsRepository, LoadToolsRepository } from '@/data/protocols'
+import { AddToolRepository, LoadToolByTagsRepository, LoadToolsRepository, RemoveToolRepository } from '@/data/protocols'
 import { ToolModel } from '@/domain/models/tool'
 import { AddToolModel } from '@/domain/usecases'
 import { MongoHelper } from './mongo-helper'
 
-export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsRepository, AddToolRepository {
+export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsRepository, AddToolRepository, RemoveToolRepository {
   async load (): Promise<ToolModel[]> {
     const toolCollection = await MongoHelper.getCollection('tools')
     const tools = await toolCollection.find().toArray()
@@ -20,5 +20,10 @@ export class ToolMongoRepository implements LoadToolsRepository, LoadToolByTagsR
     const toolCollection = await MongoHelper.getCollection('tools')
     const tool = (await toolCollection.insertOne(toolData)).ops[0]
     return MongoHelper.map(tool)
+  }
+
+  async remove (id: string): Promise<void> {
+    const toolCollection = await MongoHelper.getCollection('tools')
+    await toolCollection.deleteOne({ _id: id })
   }
 }
