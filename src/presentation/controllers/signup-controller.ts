e91c9fdd6caaef506e6867fe17c8a5c9d18d14f0
Flavio Duarte <flduarte13@gmail.com>
2021-01-21 07:22:46 -0300

    feat: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,6 +1,6 @@
 import { AddAccount } from '@/domain/usecases'
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
-import { badRequest, forbidden } from '@/presentation/helpers'
+import { badRequest, forbidden, serverError } from '@/presentation/helpers'
 import { EmailInUseError } from '@/validation/errors/email-in-use-error'
 
 export class SignUpController implements Controller {
@@ -10,19 +10,23 @@ export class SignUpController implements Controller {
   ) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(request.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(request.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { email, name, password } = request.body
+      const account = await this.addAccount.add({
+        email,
+        name,
+        password
+      })
+      if (!account) {
+        return forbidden(new EmailInUseError())
+      }
+      return null
+    } catch {
+      return serverError()
     }
-    const { email, name, password } = request.body
-    const account = await this.addAccount.add({
-      email,
-      name,
-      password
-    })
-    if (!account) {
-      return forbidden(new EmailInUseError())
-    }
-    return null
   }
 }
