42eb89a82df0756a165ae55b3ee273ed49a7536e
Flavio Duarte <flduarte13@gmail.com>
2021-01-27 20:16:00 -0300

    feat: ensure BcryptAdapter.compare returns true when compare succeed

--- src/infra/db/cryptography/bcrypt-adapter.ts
+++ src/infra/db/cryptography/bcrypt-adapter.ts
@@ -12,7 +12,6 @@ export class BcryptAdapter implements Hasher, HashComparer {
   }
 
   async compare (value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return null
+    return await bcrypt.compare(value, hash)
   }
 }
