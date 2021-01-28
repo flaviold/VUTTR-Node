9007dd3a9615ac056bace4eb8b3d5fd34ceca264
Flavio Duarte <flduarte13@gmail.com>
2021-01-28 20:55:43 -0300

    test: ensure EmailValidation returns an error if EmailValidator returns false

--- tests/validation/validators/email-validation.spec.ts
+++ tests/validation/validators/email-validation.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '@/validation/errors'
 import { EmailValidation } from '@/validation/validators'
 import { EmailValidatorSpy } from '@/tests/validation/mocks/mock-email-validatio'
 
@@ -23,5 +24,13 @@ describe('EmailValidation', () => {
     const email = faker.internet.email()
     sut.validate({ email })
     expect(emailValidatorSpy.email).toBe(email)
+  })
+
+  test('Should return an error if EmailValidator returns false', () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const email = faker.internet.email()
+    emailValidatorSpy.isEmailValid = false
+    const error = sut.validate({ email })
+    expect(error).toEqual(new InvalidParamError('email'))
   })
 })
